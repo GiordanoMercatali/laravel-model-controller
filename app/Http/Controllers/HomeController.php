d9cddcde2cb709f1d1f9f0f3fffae44f2be7e396
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-01-15 16:37:13 +0100

    add: movies displayed in page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index() {
+        $movies = Movie::orderBy('title')->get();
+        return view('welcome', compact('movies'));
+    } 
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to laravel</h1>
+    <h1>Movies</h1>
+    <div class="container">
+        <ul>
+            @foreach ($movies as $movie)
+            <li class="col-2 p-2">
+                <img src="{{ $movie->image }}" alt="{{ $movie->title }}">
+                <h4 class="text-center">{{ $movie->title }}</h4>
+            </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
